f704a884f0cd432c6f2ece6bc78c335c5096eccb
Nathaniel Mwabili <nathaniel.mwabz@gmail.com>
2024-11-09 23:48:37 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,64 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/utils.css";
+
+const body = document.querySelector("body"),
+  nav = document.querySelector("nav"),
+  modeToggle = document.querySelector(".dark-light"),
+  searchToggle = document.querySelector(".searchToggle"),
+  sidebarOpen = document.querySelector(".sidebarOpen"),
+  sidebarClose = document.querySelector(".sidebarClose");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark-mode") {
+  body.classList.add("dark");
+}
+
+/* code to toggle dark and light mode */
+modeToggle.addEventListener("click", () => {
+  modeToggle.classList.toggle("active");
+  body.classList.toggle("dark");
+
+  //code to keep mode even when page is selected
+  if (!body.classList.contains("dark")) {
+    localStorage.setItem("mode", "light-mode");
+  } else {
+    localStorage.setItem("mode", "dark-mode");
+  }
+});
+
+/* code to toggle search box */
+searchToggle.addEventListener("click", () => {
+  searchToggle.classList.toggle("active");
+});
+
+//code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+  nav.classList.add("active");
+});
+body.addEventListener("click", (e) => {
+  let clickedElm = e.target;
+
+  if (
+    !clickedElm.classList.contains("sidebarOpen") &&
+    !clickedElm.classList.contains("menu")
+  )
+    nav.classList.remove("active");
+});
+
+//observer for scroll animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
