999c49a3e8a2d4452c06b1b1c34c79d1a05ebe60
Nathaniel Mwabili <nathaniel.mwabz@gmail.com>
2024-11-25 01:06:15 +0300

    First Commit

--- NatesPortfolio/src/nate.js
+++ NatesPortfolio/src/nate.js
@@ -3,4 +3,6 @@ import "../styles/nate.css";
 import "../styles/components/background.css";
 import "../styles/components/header.css";
 import "../styles/components/about.css";
+import "../styles/components/experience.css";
+import "../styles/components/project.css";
 import "../styles/utils.css";
